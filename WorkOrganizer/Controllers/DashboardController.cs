d56f4f1c6c86da94701b9cf2a40db81417993e25
Anders <drewn@live.se>
2019-04-09 11:34:43 +0200

    AddDashboardController

---
+++ WorkOrganizer/Controllers/DashboardController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WorkOrganizer.Data;
+using WorkOrganizer.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ProMan.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dashboard
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Project.ToListAsync());
+        }
+
+        // GET: Dashboard/Projects
+        public async Task<IActionResult> Projects()
+        {
+            return View(await _context.Project.ToListAsync());
+        }
+
+        // GET: Dashboard/ProjectDetails/5
+        public async Task<IActionResult> ProjectDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Project
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // GET: Dashboard/ProjectCreate
+        public IActionResult CreateProject()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/CreateProject
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProject([Bind("Id,Name,StartDate,Description")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(project);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+
+        // GET: Dashboard/EditProject/5
+        public async Task<IActionResult> EditProject(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Project.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Dashboard/EditProject/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditProject(int id, [Bind("Id,Name,StartDate,Description")] Project project)
+        {
+            if (id != project.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(project);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProjectExists(project.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+
+        // GET: Dashboard/DeleteProject/5
+        public async Task<IActionResult> DeleteProject(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Project
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+        // POST: Dashboard/DeleteProject/5
+        [HttpPost, ActionName("DeleteProject")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteProjectConfirmed(int id)
+        {
+            var project = await _context.Project.FindAsync(id);
+            _context.Project.Remove(project);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Projects));
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return _context.Project.Any(e => e.Id == id);
+        }
+    }
+}
