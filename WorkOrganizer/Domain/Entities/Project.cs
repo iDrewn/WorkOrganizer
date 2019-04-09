049451ab16f7b7546410675881c9ff8183595e25
Statica01 <caritasmail@gmail.com>
2019-04-09 11:26:15 +0200

    AddEntityProject

--- WorkOrganizer/Domain/Entities/Project.cs
+++ WorkOrganizer/Domain/Entities/Project.cs
@@ -1,6 +1,12 @@
-﻿namespace WorkOrganizer.Domain.Entities
+﻿using System;
+
+namespace WorkOrganizer.Domain.Entities
 {
     public class Project
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Description { get; set; }
     }
 }
