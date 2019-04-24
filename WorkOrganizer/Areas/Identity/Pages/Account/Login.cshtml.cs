7404c4aecffcf5a1b44cb7cd3b7c0c2b707e2af8
tokas85 <tomas.lindqvist85@gmail.com>
2019-04-24 14:04:08 +0200

    Add search bar + better layout

--- WorkOrganizer/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WorkOrganizer/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -77,7 +77,7 @@ namespace WorkOrganizer.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return LocalRedirect("~/Dashboard");
                 }
                 if (result.RequiresTwoFactor)
                 {
