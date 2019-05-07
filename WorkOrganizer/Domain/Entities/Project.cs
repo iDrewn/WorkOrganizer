d69b15b5235f6ef2be6b56b89f89d80e12e17c23
Anders <drewn@live.se>
2019-05-07 17:35:24 +0200

    AddEmptyConstructor

--- WorkOrganizer/Domain/Entities/Project.cs
+++ WorkOrganizer/Domain/Entities/Project.cs
@@ -15,6 +15,11 @@ namespace WorkOrganizer.Domain.Entities
             IdentityUserId = identityUserId;
         }
 
+        public Project()
+        {
+
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
