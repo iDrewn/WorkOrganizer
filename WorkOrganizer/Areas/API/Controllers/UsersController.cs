9d8c3a8c8595325e0df9fbf171188ef78b3140c4
EmmaJ <johannessonemma@hotmail.com>
2019-05-14 12:58:38 +0200

    Fix UserController API

---
+++ WorkOrganizer/Areas/API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkOrganizer.Areas.API.Models;
+using WorkOrganizer.Areas.API.Services;
+using WorkOrganizer.Domain.Services;
+
+namespace WorkOrganizer.Areas.API.Controllers                    
+{
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IApplicationUserService applicationUserService;
+
+        public UsersController(IApplicationUserService applicationUserService)
+        {
+            this.applicationUserService = applicationUserService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> CreateUserAsync(UserDto userDto)
+        {
+            var user = await applicationUserService.CreateUserAsync(userDto.Email, userDto.Password, userDto.Name, userDto.Firstname, userDto.Lastname, userDto.SocialSecurityNumber);
+
+            return Ok(user);
+        }
+    }
+}
