973c9f52eaed2ca70ecf3627124f47e0da110ae7
Dawid <dawid_owizyc@hotmail.com>
2019-06-04 11:56:11 +0200

    add inviting

---
+++ WorkOrganizer/Domain/Entities/ProjectMember.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkOrganizer.Domain.Entities
+{
+    public class ProjectMember
+    {
+        public int Id { get; set; }
+        public Project Project { get; set; }
+        public int ProjectId { get; set; }
+        public bool IsAdmin { get; set; }
+        public ApplicationUser Member { get; set; }
+        public string MemberId { get; set; }
+        
+
+    }
+}
