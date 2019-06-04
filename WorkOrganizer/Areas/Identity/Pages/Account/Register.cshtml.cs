b6f1feebfef3ae0fddb281931a342c8534538009
Tomas <tomas.lindqvist85@gmail.com>
2019-06-04 10:42:44 +0200

    Add extremle gut design. Much nice. Yes.

--- WorkOrganizer/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WorkOrganizer/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -60,7 +60,7 @@ namespace WorkOrganizer.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
-            [Required]
+            //[Required]
             [Display(Name = "Title ex. Admin or User")]
             public string Name { get; set; }
 
